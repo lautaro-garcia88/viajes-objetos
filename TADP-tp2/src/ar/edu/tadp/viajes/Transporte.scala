0bcf3bf12ce929be95cbb69992f25d6f372f4434
cesarvera <cesar.vera96@gmail.com>
2014-07-07 21:35:03 -0300

    borro codigo inservible

--- TADP-tp2/src/ar/edu/tadp/viajes/Transporte.scala
+++ TADP-tp2/src/ar/edu/tadp/viajes/Transporte.scala
@@ -3,8 +3,6 @@ package ar.edu.tadp.viajes
 abstract class Transporte {
   def calcularTarifa(origen: Direccion, destino: Direccion): Double
 
-  def calcularDistancia(origen: Direccion, destino: Direccion): Int = { return 0 }
-
   def calcularTiempo(origen: Direccion, destino: Direccion): Double
 
   def esIgual(unTransporte: Transporte): Boolean
