69c902a9989e92c90093dc3bcd62cc9d915ed093
cesarvera <cesar.vera96@gmail.com>
2014-07-11 21:57:52 -0300

    agrego la funcionalidad de consulta de estadisticas

--- TADP-tp2/src/ar/edu/tadp/viajes/Consulta.scala
+++ TADP-tp2/src/ar/edu/tadp/viajes/Consulta.scala
@@ -1,5 +1,36 @@
 package ar.edu.tadp.viajes
 
-class Consulta {
+class Consulta(var viajes: List[Viaje]) {
+  def costoPromedioViajes(): Double = {
+    var costoTotal: Double = 0
+    
+    for (unViaje <- viajes) {
+      costoTotal = costoTotal + unViaje.calcularCosto()
+    }
 
+    return costoTotal / viajes.length
+  }
+
+  def tiempoPromedioViajes(): Double = {
+    var tiempoTotal: Double = 0
+
+    for (unViaje <- viajes) {
+      tiempoTotal = tiempoTotal + unViaje.calcularTiempo()
+    }
+    return tiempoTotal / viajes.length
+  }
+
+  def porcentajeUtilizacion(): Double = {
+    return viajes.length
+  }
+
+  def facturacionTotal(): Double = {
+    var costoTotal: Double = 0
+
+    for (unViaje <- viajes) {
+      costoTotal = costoTotal + unViaje.calcularCosto()
+    }
+
+    return costoTotal
+  }
 }
